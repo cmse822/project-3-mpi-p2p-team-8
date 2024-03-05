deae10274a77b40163d4951f7836746c49a0f374
jreiling19 <jreiling19@augustana.edu>
2024-03-05 11:40:28 -0500

    changed to char for buffer

--- blockingPingPong.cpp
+++ blockingPingPong.cpp
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
-<<<<<<< Updated upstream
     int processA,processB;
      processA = 0; processB = 1;
 
@@ -54,45 +53,6 @@ int main(int argc, char *argv[])
     
     double total_time = end_time - start_time;
     printf("Time to complete the ping-pong exchange with message size %d: %d", length, total_time);
-=======
-    double send_data = 1.;
-    // TODO: init start_node
-    // TODO: init end_node (We should know where the end node is)
-    // for node in nodes
-        // TODO: Halt the start_node+1
-        // TODO: Pass the data to start_node+1
-        // TODO: update the start_node
-    if ( /*I am process A*/) {
-    // TODO: 
-        MPI_Send (/*to process B*/ )
-        MPI_Recv(/*from process B*/)
-    } else {
-        MPI_Recv(/*from process A*/)
-        MPI_Send(/*to process A*/)
-    }
-
-    /*
-    start_node = 0
-    num_nodes = 2 // to N
-
-    for (int i = 0; i < num_nodes-1; i++) {
-        if (rank == start_node) {
-
-            MPI_Send(&send_data, 1, MPI_DOUBLE, start_node+1, 0, MPI_COMM_WORLD);
-
-            MPI_Recv(&send_data, 1, MPI_DOUBLE, start_node, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        } else if (rank == num_nodes-1) {
-            MPI_Send(&send_data, 1, MPI_DOUBLE, start_node, 0, MPI_COMM_WORLD);
-            MPI_Recv(&send_data, 1, MPI_DOUBLE, start_node+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-        start_node++;
-    }
-
-// TODO: In the time/byte graph, moment the curve starts to change is the moment we hit the bandwidth limit.
-    */
-
->>>>>>> Stashed changes
     MPI_Finalize();
     return 0;
 }

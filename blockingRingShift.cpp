56eb637a4d0d88e10909045256d76bf5f45c4d33
Berk Atik <berkatik@Berks-MBP.attlocal.net>
2024-03-08 16:16:35 -0500

    feat: Part 3.1 final

--- blockingRingShift.cpp
+++ blockingRingShift.cpp
@@ -8,38 +8,35 @@ int main(int argc, char *argv[]) {
     int length = 1;
     char *sendbuf = (char *)malloc(length * sizeof(char));
     char *recvbuf = (char *)malloc(length * sizeof(char));
+    int next_rank;
+    int prev_rank;
+
     double start_time = 0;
     double end_time = 0;
-    int token = 0;
 
     MPI_Init(&argc, &argv);
 
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (rank == 0){
-        token = 1;
-    }
+    int send_token = rank;
+    int recv_token;
 
-    for (step_idx = 0; step_idx < numtasks; step_idx++)
-    {
-        if (step_idx != rank)
-        {
-            
-        }
-        int next_rank = (rank + 1) % numtasks;
-        int prev_rank = (rank - 1 + numtasks) % numtasks;
+    next_rank = (rank + 1) % numtasks;
+    prev_rank = (rank + numtasks - 1) % numtasks;
 
-        int recv_token;
 
-        start_time = MPI_Wtime();
-        MPI_Sendrecv(&token, 1, MPI_INT, next_rank, 0, &recv_token, 1, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        end_time = MPI_Wtime();
+    MPI_Barrier(MPI_COMM_WORLD);
 
-        printf("Rank %d received token %d from rank %d\n", rank, recv_token, prev_rank);
-        printf("Rank %d sending token %d to rank %d\n", rank, recv_token, next_rank);
-        printf("\n");
-    }
+    start_time = MPI_Wtime();
+
+    MPI_Sendrecv(&send_token, 1, MPI_INT, next_rank, 0, &recv_token, 1, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    printf("Rank %d received token %d from rank %d\n", rank, recv_token, prev_rank);
+    printf("Rank %d sending token %d to rank %d\n", rank, send_token, next_rank);
+    printf("\n");
+
+    end_time = MPI_Wtime();
 
     double total_time = end_time - start_time;
     printf("Time to complete the ring %d: %f\n", length, total_time);
